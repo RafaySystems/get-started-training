4fb2d87d0c2b3811102c5728a7f0b507f1f8520f
umair-rafay <119533491+umair-rafay@users.noreply.github.com>
2023-08-15 16:15:36 -0400

    Update providers.tf

--- terraform/pas_terraform/modules/aks/providers.tf
+++ terraform/pas_terraform/modules/aks/providers.tf
@@ -2,7 +2,18 @@ terraform {
   required_providers {
     rafay = {
       source  = "RafaySystems/rafay"
-      version = ">= 1.0.0"
+      version =  ">= 1.0.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
     }
   }
+}
+
+provider "rafay" {
+  provider_config_file = var.rafay_config_file
+}
+provider "azurerm" {
+  features {}
 }
